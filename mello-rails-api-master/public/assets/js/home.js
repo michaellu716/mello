05dde3f7d9f0fdbdd869df34471faf13ab9148e0
Michael Lu <michael_108@hotmail.com>
2019-10-10 20:34:32 +0000

    finished mello 17

--- mello-rails-api-master/public/assets/js/home.js
+++ mello-rails-api-master/public/assets/js/home.js
@@ -1,22 +1,62 @@
+const $submitButton = $('#submit');
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
-const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
 function setAuth(setting) {
-    authSetting = setting;
-  
-    if (authSetting === 'login') {
-      $setLogin.addClass('active');
-      $setSignUp.removeClass('active');
-      $submitButton.text('Log In');
-    } else {
-      $setSignUp.addClass('active');
-      $setLogin.removeClass('active');
-      $submitButton.text('Sign Up');
-    }
+  authSetting = setting;
+
+  if (authSetting === 'login') {
+    $setLogin.addClass('active');
+    $setSignUp.removeClass('active');
+    $submitButton.text('Log In');
+  } else {
+    $setSignUp.addClass('active');
+    $setLogin.removeClass('active');
+    $submitButton.text('Sign Up');
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger');
+    return;
+  }
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting,
+    data: {
+      user: {
+        email,
+        password
+      }
+    },
+    method: 'POST'
+  }).then(function(data) {
+    console.log(data);
+  });
 }
 
 $setLogin.on('click', setAuth.bind(null, 'login'));
 $setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
